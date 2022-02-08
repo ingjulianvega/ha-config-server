f2fd9b1d6af058a9755fb7a0297dd3a786a6019d
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-02-08 15:49:38 -0500

    ✨ Initial commit

---
+++ src/test/java/ingjulianvega/ximic/haconfigserver/HaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.haconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
